c5342affac434619f167ec7259ba7e13d3ebfea2
Stephen Mullen <stephen@wirelessguy.co.uk>
2013-03-13 03:01:16 +0000

    Updated Conditional Stylesheets vs CSS Hack
    http://paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/

--- header.php
+++ header.php
@@ -1,11 +1,9 @@
 <!DOCTYPE html>
-
 <!-- paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/ -->
 <!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" <?php language_attributes(); ?>> <![endif]-->
 <!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" <?php language_attributes(); ?>> <![endif]-->
 <!--[if IE 8]>    <html class="no-js lt-ie9" <?php language_attributes(); ?>> <![endif]-->
-<!--[if gt IE 8]><!-->
-<html <?php language_attributes(); ?>> <!--<![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" <?php language_attributes(); ?>> <!--<![endif]-->
 <head>
 	<meta charset="utf-8" />
 	
@@ -68,7 +66,4 @@
           ) );
       ?>
   </section>
-</nav>		
-		<!-- Header Row -->
-		<!-- Header Row -->
-		
+</nav>
