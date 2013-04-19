77f2d4cfe432ac3cc697efa618f582a8be380c06
Stephen Mullen <stephen@wirelessguy.co.uk>
2013-04-19 04:20:00 +0100

    Added additional classes for styling.

--- page.php
+++ page.php
@@ -1,41 +1,28 @@
 <?php get_header(); ?>
 
-<!-- Main Row -->
 <div class="row">
-<div class="large-8 columns">
+	<div class="large-8 columns" role="main">
 
-<!-- Skip Nav -->
-<a id="content"></a>
+		<?php while ( have_posts() ) : the_post(); ?>
 
-	<!-- Start the Loop -->
-	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-	
-		<article>
-				
-			<h1><?php the_title(); ?></h1>
-			
-			<!-- Display the Page's Content in a div box. -->
-			<div class="entry">
-				<?php the_content(); ?>
-			</div>
-		
-		</article>
-	
-	<!-- Stop The Loop (but note the "else:" - see next line). -->
-	<?php endwhile; else: ?>
-	
-		<!-- The very first "if" tested to see if there were any Posts to -->
-		<!-- display.  This "else" part tells what do if there weren't any. -->
-		<div class="alert-box error">Sorry, the page you requested was not found</div>
-	
-	<!--End the loop -->
-	<?php endif; ?>
-	
+			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+				<header class="entry-header">
+					<h1 class="entry-title"><?php the_title(); ?></h1>
+				</header>
+
+				<div class="entry-content">
+					<?php the_content(); ?>
+				</div>
+
+			</article>
+
+		<?php endwhile; ?>
+
+	</div>
+
+	<?php get_sidebar(); ?>
+
 </div>
-	
-<?php get_sidebar(); ?>
-		
-</div>
-<!-- Main Row -->
 
 <?php get_footer(); ?>
