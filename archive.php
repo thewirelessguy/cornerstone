b3e9e3ba6fa12175bab6ae43574e6af463cf8a61
Stephen Mullen <stephen@wirelessguy.co.uk>
2013-06-12 05:27:19 +0100

    Update archive.php

--- archive.php
+++ archive.php
@@ -1,39 +1,67 @@
-<?php 
-/*
-Template Name: Archives
-*/
+<?php
+/**
+ * The template for displaying Archive pages.
+ *
+ * Used to display archive-type pages if nothing more specific matches a query.
+ * For example, puts together date-based pages if no date.php file exists.
+ *
+ * If you'd like to further customize these archive views, you may create a
+ * new template file for each specific one. For example, Twenty Twelve already
+ * has tag.php for Tag archives, category.php for Category archives, and
+ * author.php for Author archives.
+ *
+ * Learn more: http://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twelve
+ * @since Twenty Twelve 1.0
+ */
 
 get_header();
 
 ?>
 
-<!-- archives -->
-
-<!-- Main Row -->
 <div class="row">
 
-<div class="large-8 columns">
+	<div id="content" class="large-8 columns" role="main">
 
-	<?php the_post(); ?>
-	<h2 class="entry-title"><?php the_title(); ?></h2>
+		<?php if ( have_posts() ) : ?>
+			<header class="archive-header">
+				<h1 class="archive-title"><?php
+					if ( is_day() ) :
+						printf( __( 'Daily Archives: %s', 'cornerstone' ), '<span>' . get_the_date() . '</span>' );
+					elseif ( is_month() ) :
+						printf( __( 'Monthly Archives: %s', 'cornerstone' ), '<span>' . get_the_date( _x( 'F Y', 'monthly archives date format', 'cornerstone' ) ) . '</span>' );
+					elseif ( is_year() ) :
+						printf( __( 'Yearly Archives: %s', 'cornerstone' ), '<span>' . get_the_date( _x( 'Y', 'yearly archives date format', 'cornerstone' ) ) . '</span>' );
+					else :
+						_e( 'Archives', 'cornerstone' );
+					endif;
+				?></h1>
+			</header><!-- .archive-header -->
 
-	<h4 class="subheader">Archives by Month:</h4>
-	<ul>
-		<?php wp_get_archives('type=monthly'); ?>
-	</ul>
+			<?php
+			/* Start the Loop */
+			while ( have_posts() ) : the_post();
 
-	<h4 class="subheader">Archives by Subject:</h4>
-	<ul>
-		 <?php wp_list_categories(); ?>
-	</ul>
+				/* Include the post format-specific template for the content. If you want to
+				 * this in a child theme then include a file called called content-___.php
+				 * (where ___ is the post format) and that will be used instead.
+				 */
+				get_template_part( 'content', get_post_format() );
+
+			endwhile;
+
+			?>
+
+		<?php else : ?>
+			<?php get_template_part( 'content', 'none' ); ?>
+		<?php endif; ?>
+
+	</div>
+
+	<?php get_sidebar(); ?>
 
 </div>
-
-<?php get_sidebar(); ?>
-
-</div>
-<!-- Main Row -->
 
 <?php get_footer(); ?>
-
-<!-- archives -->
