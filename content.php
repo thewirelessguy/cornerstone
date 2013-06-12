971a9d744ec44cc2f6f6d4ba893de2f1fb2bb4b9
Stephen Mullen <stephen@wirelessguy.co.uk>
2013-06-12 05:20:27 +0100

    Change h1 tag to h2 on index/archive

--- content.php
+++ content.php
@@ -17,10 +17,12 @@
 	<header class="entry-header">
 		<?php if ( is_single() ) : ?>
 		<h1 class="entry-title"><?php the_title(); ?></h1>
-		<?php else : ?>
-		<h1 class="entry-title">
 			<a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', 'cornerstone' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_title(); ?></a>
 		</h1>
+		<?php else : ?>
+		<h2 class="entry-title">
+			<a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', 'cornerstone' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_title(); ?></a>
+		</h2>
 		<?php endif; // is_single() ?>
 	</header>
 
