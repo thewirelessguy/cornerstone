50ba3e8700b061f5d8bef8e9885b62b5d758e74a
Courtney Elizabeth <freak.couture@gmail.com>
2013-06-20 08:51:29 -0600

    Added Shortcodes to shortcode.php

--- inc/shortcodes.php
+++ inc/shortcodes.php
@@ -2,4 +2,62 @@
 
 // Shortcodes
 
+// Allow shortcodes in widgets
+
+add_filter('widget_text', 'do_shortcode');
+
+// Rows [row][/row]
+
+function shortcode_row( $atts, $content = null ) {
+   return '<div class="row">' . do_shortcode($content) . '</div>';
+}
+
+add_shortcode( 'row', 'shortcode_row' );
+
+// Columns [column][/column]
+
+function shortcode_column( $atts, $content = null ) {
+
+  extract( shortcode_atts( array(
+  	'center' => '',
+		'span' => '',
+		), $atts ) );
+
+	// Set the 'center' variable
+	if ($center == 'true') {
+	$center = 'centered';
+	}
+
+	return '<div class="' . esc_attr($span) . ' columns ' . esc_attr($center) .'">' . do_shortcode($content) . '</div>';
+}
+
+add_shortcode( 'column', 'shortcode_column' );
+
+// Sections [sections type="tabs"] [section title="Section Title"]Content[/section] [/sections]
+
+function shortcode_sections( $atts, $content = null ) {
+
+  extract( shortcode_atts( array(
+		'type' => ''
+		), $atts ) );
+
+	return '<div class="section-container '. esc_attr($type) . '" data-section="'. esc_attr($type) . '">' . do_shortcode($content) . '</div>';
+
+}
+
+add_shortcode( 'sections', 'shortcode_sections' );
+
+// Section [section title="Section Title"]Content[/section]
+
+function shortcode_section( $atts, $content = null ) {
+
+	extract( shortcode_atts( array(
+		'title' => ''
+		), $atts ) );
+
+	return '<section><p class="title" data-section-title><a href="#">'. esc_attr($title) . '</a></p><div class="content" data-section-content>' . do_shortcode($content) . '</div></section>';
+
+}
+
+add_shortcode( 'section', 'shortcode_section' );
 ?>
