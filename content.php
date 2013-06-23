2c88ec3bf0cb4966c44af793429111ff5759a196
Stephen Mullen <stephen@wirelessguy.co.uk>
2013-06-23 14:12:05 +0100

    Removed stray permalink after the h1 title tag

--- content.php
+++ content.php
@@ -17,8 +17,6 @@
 	<header class="entry-header">
 		<?php if ( is_single() ) : ?>
 		<h1 class="entry-title"><?php the_title(); ?></h1>
-			<a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', 'cornerstone' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_title(); ?></a>
-		</h1>
 		<?php else : ?>
 		<h2 class="entry-title">
 			<a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', 'cornerstone' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_title(); ?></a>
