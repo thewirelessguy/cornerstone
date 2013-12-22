9164b0750e46c4d0fc7f0d55b4cd576604e64597
Stephen Mullen <stephen@wirelessguy.co.uk>
2013-12-22 04:22:27 +0000

    Update comments functions

--- functions.php
+++ functions.php
@@ -148,65 +148,71 @@ if (function_exists('register_sidebar')) {
 	));
 }
 
+
 // Comments
 
-// Custom callback to list comments in the Foundation style
-function custom_comments($comment, $args, $depth) {
-  $GLOBALS['comment'] = $comment;
-    $GLOBALS['comment_depth'] = $depth;
-  ?>
-    <li id="comment-<?php comment_ID() ?>" <?php comment_class() ?>>
-        <div class="comment-author vcard"><?php commenter_link() ?></div>
-        <div class="comment-meta"><?php printf(__('Posted %1$s at %2$s <span class="meta-sep">|</span> <a href="%3$s" title="Permalink to this comment">Permalink</a>', 'Foundation'),
-                    get_comment_date(),
-                    get_comment_time(),
-                    '#comment-' . get_comment_ID() );
-                    edit_comment_link(__('Edit', 'Foundation'), ' <span class="meta-sep">|</span> <span class="edit-link">', '</span>'); ?></div>
-  <?php if ($comment->comment_approved == '0') _e("\t\t\t\t\t<span class='unapproved'>Your comment is awaiting moderation.</span>\n", 'Foundation') ?>
-          <div class="comment-content">
-            <?php comment_text() ?>
-        </div>
-        <?php // echo the comment reply link
-            if($args['type'] == 'all' || get_comment_type() == 'comment') :
-                comment_reply_link(array_merge($args, array(
-                    'reply_text' => __('Reply','Foundation'),
-                    'login_text' => __('Log in to reply.','Foundation'),
-                    'depth' => $depth,
-                    'before' => '<div class="comment-reply-link">',
-                    'after' => '</div>'
-                )));
-            endif;
-        ?>
-<?php } // end custom_comments
+if ( ! function_exists( 'cornerstone_comment' ) ) :
+/**
+ * Template for comments and pingbacks.
+ *
+ * To override this walker in a child theme without modifying the comments template
+ * simply create your own cornerstone_comment(), and that function will be used instead.
+ *
+ * Used as a callback by wp_list_comments() for displaying the comments.
+ *
+ */
+function cornerstone_comment( $comment, $args, $depth ) {
+	$GLOBALS['comment'] = $comment;
+	switch ( $comment->comment_type ) :
+		case 'pingback' :
+		case 'trackback' :
+		// Display trackbacks differently than normal comments.
+	?>
+	<li <?php comment_class(); ?> id="comment-<?php comment_ID(); ?>">
+		<p><?php _e( 'Pingback:', 'cornerstone' ); ?> <?php comment_author_link(); ?> <?php edit_comment_link( __( '(Edit)', 'cornerstone' ), '<span class="edit-link">', '</span>' ); ?></p>
+	<?php
+			break;
+		default :
+		// Proceed with normal comments.
+		global $post;
+	?>
+	<li <?php comment_class(); ?> id="li-comment-<?php comment_ID(); ?>">
+		<article id="comment-<?php comment_ID(); ?>" class="comment">
+			<header class="comment-meta comment-author vcard">
+				<?php
+					echo get_avatar( $comment, 44 );
+					printf( '<cite><b class="fn">%1$s</b> %2$s</cite>',
+						get_comment_author_link(),
+						// If current post author is also comment author, make it known visually.
+						( $comment->user_id === $post->post_author ) ? '<span>' . __( 'Post author', 'cornerstone' ) . '</span>' : ''
+					);
+					printf( '<a href="%1$s"><time datetime="%2$s">%3$s</time></a>',
+						esc_url( get_comment_link( $comment->comment_ID ) ),
+						get_comment_time( 'c' ),
+						/* translators: 1: date, 2: time */
+						sprintf( __( '%1$s at %2$s', 'cornerstone' ), get_comment_date(), get_comment_time() )
+					);
+				?>
+			</header><!-- .comment-meta -->
 
-// Custom callback to list pings
-function custom_pings($comment, $args, $depth) {
-       $GLOBALS['comment'] = $comment;
-        ?>
-            <li id="comment-<?php comment_ID() ?>" <?php comment_class() ?>>
-                <div class="comment-author"><?php printf(__('By %1$s on %2$s at %3$s', 'Foundation'),
-                        get_comment_author_link(),
-                        get_comment_date(),
-                        get_comment_time() );
-                        edit_comment_link(__('Edit', 'Foundation'), ' <span class="meta-sep">|</span> <span class="edit-link">', '</span>'); ?></div>
-    <?php if ($comment->comment_approved == '0') _e('\t\t\t\t\t<span class="unapproved">Your trackback is awaiting moderation.</span>\n', 'Foundation') ?>
-            <div class="comment-content">
-                <?php comment_text() ?>
-            </div>
-<?php } // end custom_pings
+			<?php if ( '0' == $comment->comment_approved ) : ?>
+				<p class="comment-awaiting-moderation"><?php _e( 'Your comment is awaiting moderation.', 'cornerstone' ); ?></p>
+			<?php endif; ?>
 
-// Produces an avatar image with the hCard-compliant photo class
-function commenter_link() {
-    $commenter = get_comment_author_link();
-    if ( ereg( '<a[^>]* class=[^>]+>', $commenter ) ) {
-        $commenter = ereg_replace( '(<a[^>]* class=[\'"]?)', '\\1url ' , $commenter );
-    } else {
-        $commenter = ereg_replace( '(<a )/', '\\1class="url "' , $commenter );
-    }
-    $avatar_email = get_comment_author_email();
-    $avatar = str_replace( "class='avatar", "class='photo avatar", get_avatar( $avatar_email, 35 ) );
-    echo $avatar . ' <span class="fn n">' . $commenter . '</span>';
-} // end commenter_link
+			<section class="comment-content comment">
+				<?php comment_text(); ?>
+				<?php edit_comment_link( __( 'Edit', 'cornerstone' ), '<p class="edit-link">', '</p>' ); ?>
+			</section><!-- .comment-content -->
+
+			<div class="reply">
+				<?php comment_reply_link( array_merge( $args, array( 'reply_text' => __( 'Reply', 'cornerstone' ), 'after' => ' <span>&darr;</span>', 'depth' => $depth, 'max_depth' => $args['max_depth'] ) ) ); ?>
+			</div><!-- .reply -->
+		</article><!-- #comment-## -->
+	<?php
+		break;
+	endswitch; // end comment_type check
+}
+endif;
 
 
 // Custom Pagination
