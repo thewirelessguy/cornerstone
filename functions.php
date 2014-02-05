a4fb675e7dc9cbb32b1d60c5959c55c185be4a17
Stephen Mullen <stephen@wirelessguy.co.uk>
2014-02-05 22:04:52 +0000

    Update to Foundation 5.1

--- functions.php
+++ functions.php
@@ -12,6 +12,9 @@ add_theme_support( 'post-formats', array( 'aside', 'image', 'link', 'quote', 'st
 // Add thumbnail support
 add_theme_support( 'post-thumbnails' );
 
+// Add widget live previews support
+add_theme_support(‘widget-customizer’);
+
 // Disable the admin bar, set to true if you want it to be visible.
 show_admin_bar(FALSE);
 
@@ -42,7 +45,7 @@ if ( ! function_exists( 'load_cornerstone_css' ) ) {
 			'foundation_css',
 			get_template_directory_uri() . '/css/foundation.min.css',
 			array('normalize'),
-			'5.0.3',
+			'5.1',
 			'all'
 		);
 
@@ -66,9 +69,9 @@ if ( ! function_exists( 'load_cornerstone_scripts' ) ) {
 
 		wp_enqueue_script(
 			'foundation_modernizr_js',
-			get_template_directory_uri() . '/js/custom.modernizr.js',
+			get_template_directory_uri() . '/js/modernizr.js',
 			array(),
-			'2.6.2',
+			'2.7.1',
 			false
 		);
 
@@ -76,7 +79,7 @@ if ( ! function_exists( 'load_cornerstone_scripts' ) ) {
 			'foundation_js',
 			get_template_directory_uri() . '/js/foundation.min.js',
 			array('jquery'),
-			'5.0.3',
+			'5.1',
 			true
 		);
 
