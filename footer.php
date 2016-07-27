8ed6c01a55e84f3feb9a56d44183f8194332eafe
Stephen Mullen <stephen@wirelessguy.co.uk>
2016-07-27 19:52:34 +0100

    Fixed unclosed div tag
    Fixed unclosed div tag for <div class="off-canvas-content"
    data-off-canvas-content>

--- footer.php
+++ footer.php
@@ -1,19 +1,20 @@
-		<div id="footer">
-			<footer class="row" role="contentinfo">
-				<?php do_action( 'cornerstone_before_footer' ); ?>
-				<?php if (is_active_sidebar('footer_sidebar')) { ?>
-					<div class="row small-up-1 medium-up-2 large-up-4">
-						<?php dynamic_sidebar('footer_sidebar'); ?>
-					</div>
-				<?php } ?>
-				<?php if ( has_nav_menu( 'footer-menu' ) ) {
-					echo '<div class="row">';
-					wp_nav_menu( array( 'theme_location' => 'footer-menu', 'menu_class' => 'menu', 'container' => 'nav', 'container_class' => 'small-12 medium-12 columns' ) );
-					echo '</div>';
-				} ?>
-				<?php do_action( 'cornerstone_after_footer' ); ?>
-			</footer>
-		</div>
+			<div id="footer">
+				<footer class="row" role="contentinfo">
+					<?php do_action( 'cornerstone_before_footer' ); ?>
+					<?php if (is_active_sidebar('footer_sidebar')) { ?>
+						<div class="row small-up-1 medium-up-2 large-up-4">
+							<?php dynamic_sidebar('footer_sidebar'); ?>
+						</div>
+					<?php } ?>
+					<?php if ( has_nav_menu( 'footer-menu' ) ) {
+						echo '<div class="row">';
+						wp_nav_menu( array( 'theme_location' => 'footer-menu', 'menu_class' => 'menu', 'container' => 'nav', 'container_class' => 'small-12 medium-12 columns' ) );
+						echo '</div>';
+					} ?>
+					<?php do_action( 'cornerstone_after_footer' ); ?>
+				</footer>
+			</div>
+		</div> <?php // close .off-canvas-content ?>
 		<?php do_action( 'cornerstone_before_closing_off_canvas_wrapper' ); ?>
 	</div> <?php // close .off-canvas-wrapper-inner ?>
 </div> <?php // close .off-canvas-wrapper ?>
